0b299adaf23d05a975e7c1b708b5ef0beb2fec52
Crowdbotics <team@crowdbotics.com>
2022-05-28 16:25:35 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,23 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "nailspa_miami_35486",
   url: "https://nailspa_miami_35486.botics.co",
   api: "https://nailspa_miami_35486.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  "@modules/login": {
+    LOGO_URL: "",
+    BACKGROUND_URL: "",
+    HOME_SCREEN_NAME: "Home Screen",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
   }
 }
